55c1aaa3090541f26f80d5d1a25af0c108213647
kazumakano <kazumakano@outlook.jp>
2022-01-10 13:48:07 +0900

    update parameter.py

--- script/parameter.py
+++ script/parameter.py
@@ -5,39 +5,36 @@ from particle_filter.script.parameter import set_params as set_pf_params
 
 
 def _set_direct_params(conf: dict) -> None:
-    global ROTATE_AX, DRIFT
+    global DRIFT, ROTATE_AX
 
-    ROTATE_AX = np.int8(conf["rotate_ax"])                       # 1: +x, 2: -x, 3: +y, 4: -y, 5: +z, 6: -z (+y is tipical)
-    DRIFT = np.float16(conf["gyro_drift"])                       # drift value of gyroscope [degree/second]
+    DRIFT = np.float16(conf["gyro_drift"])
+    ROTATE_AX = np.int8(conf["rotate_ax"])
 
 def _set_dist_params(conf: dict) -> None:
-    global STEP_LEN_COEF, STATURE, DEFAULT_SPEED, BEGIN_THRESH, POS_PEAK_THRESH, NEG_PEAK_THRESH, END_THRESH, MIN_STEP_INTERVAL, MAX_STATUS_INTERVAL
+    global DEFAULT_SPEED, MAX_STATUS_INTERVAL, MIN_STEP_INTERVAL, STATURE, STEP_LEN_COEF, BEGIN_THRESH, POS_PEAK_THRESH, NEG_PEAK_THRESH, END_THRESH
 
-    STEP_LEN_COEF = np.float16(conf["step_len_coef"])            # ratio of step length to stature
-    STATURE = np.float16(conf["stature"])                        # subject's stature [meter]
-    DEFAULT_SPEED = np.float16(conf["default_speed"])            # default subject's speed [meter/second]
-
-    BEGIN_THRESH = np.float16(conf["step_begin_acc_thresh"])     # threshold values of acceleration [G]
+    DEFAULT_SPEED = np.float16(conf["default_speed"])
+    MAX_STATUS_INTERVAL = float(conf["max_status_interval"])
+    MIN_STEP_INTERVAL = float(conf["min_step_interval"])
+    STATURE = np.float16(conf["stature"])
+    STEP_LEN_COEF = np.float16(conf["step_len_coef"])
+    BEGIN_THRESH = np.float16(conf["step_begin_acc_thresh"])
     POS_PEAK_THRESH = np.float16(conf["pos_peak_acc_thresh"])
     NEG_PEAK_THRESH = np.float16(conf["neg_peak_acc_thresh"])
     END_THRESH = np.float16(conf["step_end_acc_thresh"])
-    MIN_STEP_INTERVAL = float(conf["min_step_interval"])         # minimum interval from last step to detect new step [second]
-    MAX_STATUS_INTERVAL = float(conf["max_status_interval"])       # maximum interval from last status transition to recognize as moving [second]
 
 def _set_log_params(conf: dict) -> None:
     global WIN_SIZE
 
-    WIN_SIZE = float(conf["pdr_win_size"])                       # size of window for calculating speed and angular velocity [second]
-                                                                 # the longer window size is, the faster calculation is
-                                                                 # window disabled if 0
+    WIN_SIZE = float(conf["win_size"])
 
 def set_params(conf_file: Union[str, None] = None) -> dict:
     global ROOT_DIR
 
-    ROOT_DIR = path.join(path.dirname(__file__), "../")          # project root directory
+    ROOT_DIR = path.join(path.dirname(__file__), "../")
 
     if conf_file is None:
-        conf_file = path.join(ROOT_DIR, "config/default.yaml")    # load default config file if not specified
+        conf_file = path.join(ROOT_DIR, "config/default.yaml")
 
     conf = set_pf_params(conf_file)
     _set_direct_params(conf)
